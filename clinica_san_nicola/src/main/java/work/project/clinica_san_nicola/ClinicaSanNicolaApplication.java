88d7408fce7a3925a001121cf189b15e1e565179
varca21 <varcasianicola@libero.it>
2026-02-14 14:46:52 +0100

    modifica descrizione progetto

---
+++ clinica_san_nicola/src/main/java/work/project/clinica_san_nicola/ClinicaSanNicolaApplication.java
@@ -0,0 +1,13 @@
+package work.project.clinica_san_nicola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaSanNicolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaSanNicolaApplication.class, args);
+	}
+
+}
---
+++ clinica_san_nicola/src/test/java/work/project/clinica_san_nicola/ClinicaSanNicolaApplicationTests.java
@@ -0,0 +1,13 @@
+package work.project.clinica_san_nicola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaSanNicolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
